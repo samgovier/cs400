3842a590b41863227ab037d9b4b06bf502a11185
samgovier <samgovier@gmail.com>
2019-09-10 07:57:41 -0500

    program0, implement new interface I guess

--- BinarySearchTrees/src/BinarySearchTrees/BinarySearchTreeADT.java
+++ BinarySearchTrees/src/BinarySearchTrees/BinarySearchTreeADT.java
@@ -1,22 +1,35 @@
 package BinarySearchTrees;
 
 interface BinarySearchTreeADT<T extends Comparable<T>> {
+	// for p0, assume that no element values are null
+	
+  	// adds element to the tree maintaining BST rules...ignores duplicates
+	void insert(T element);
 
-  // adds element to the tree maintaining BST rules...ignore duplicates
-  void insert(T element);
+	// if element exists, remove from tree, maintaining BST rules
+	// otherwise do nothing
+	void remove(T element);
 
-  // if element exists, remove from tree, maintaining BST rules
-  void remove(T element);
+	// returns true if element is in the tree, false otherwise
+	boolean contains(T element);
+	
+	// returns the height of this tree (empty = 0, single node = 1, etc)
+	int getHeight();
 
-  // returns true if element is in the tree, false otherwise
-  boolean contains(T element);
+	// returns the number of elements in this tree
+	int getSize();
 
-  // returns a String representing the pre-order traversal of this tree
-  String preOrderTraversal();
+	// returns a String representing the post-order traversal of this tree
+	String preOrderTraversal();
+	
+	// returns a String representing the in-order traversal of this tree
+	String inOrderTraversal();
+	
+	// prints out this tree sideways
+	void printSideways();
 
-  // returns the height of this tree (empty = 0, root only = 1, etc)
-  int getHeight();
 
-  // returns the number of elements in this tree
-  int getSize();
+
+
 }
+

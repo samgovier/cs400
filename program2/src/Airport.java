5703169b2cf561ebe4a8fc5259dc651e30254450
Sam Govier <sgovier@epic.com>
2019-09-23 11:37:46 -0500

    program2, update Airport.java from Katie

--- program2/src/Airport.java
+++ program2/src/Airport.java
@@ -26,29 +26,57 @@
  * Represents an airport. This class is completed for you.
  */
 public class Airport {
-	private String locid;
-	private String city;
-	private String airportName;
+  private String locid;
+  private String city;
+  private String name;
 
-	/**
-	 * Constructor.
-	 * @param locid
-	 * @param city
-	 * @param airportName
-	 */
-	public Airport(String locid, String city, String airportName) {
-		this.locid = locid;
-		this.city = city;
-		this.airportName = airportName;
-	}
+  /**
+   * Constructor.
+   * 
+   * @param locid
+   * @param city
+   * @param name
+   */
+  public Airport(String locid, String city, String airportName) {
+    this.locid = locid;
+    this.city = city;
+    this.name = airportName;
+  }
 
-	/**
-	 * Overrides the default toString() method so that an airport object may be printed.
-	 * Usage: for an Airport object named airport, System.out.print(airport) will print the following to standard output:
-	 * [AIRPORT ID]: [AIRPORT NAME], [AIRPORT CITY]
-	 */
-	@Override
-	public String toString() {
-		return locid + ": " + airportName + ", " + city;
-	}
+  /**
+   * Gets airport id.
+   * 
+   * @return
+   */
+  public String getId() {
+    return locid;
+  }
+
+  /**
+   * Gets airport city.
+   * 
+   * @return
+   */
+  public String getCity() {
+    return city;
+  }
+
+  /**
+   * Gets airport name.
+   * 
+   * @return
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Overrides the default toString() method so that an airport object may be printed. Usage: for an
+   * Airport object named airport, System.out.print(airport) will print the following to standard
+   * output: [AIRPORT ID]: [AIRPORT NAME], [AIRPORT CITY]
+   */
+  @Override
+  public String toString() {
+    return locid + ": " + name + ", " + city;
+  }
 }

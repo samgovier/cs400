922dea76698801603c1f5fea3df917d74f93f832
samgovier <samgovier@gmail.com>
2019-09-29 17:33:34 -0500

    program3, initial writing and formatting

--- program3/src/Program.java
+++ program3/src/Program.java
@@ -3,6 +3,14 @@ public class Program {
 
   public static void main(String[] args) {
     System.out.println("Hump mercredi");
+    Integer test = null;
+    System.out.println(test);
+    RedBlackTree<Integer, String> months = new RedBlackTree<Integer, String>();
+
+    months.insert(5, "MAY");
+    months.insert(8, "AUGUST");
+    months.insert(10, "OCTOBER");
+    months.printSideways(); // remove when submitting to ZyBooks
 
   }
 
--- program3/src/WordCountRunner.java
+++ program3/src/WordCountRunner.java
@@ -2,66 +2,107 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-////////////////////ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
 //
-//Title: Red Black Tree
-//Files: SearchTreeADT.java, RedBlackTree.java, IllegalKeyException.java, WordCountRunner.java
-//Course: Computer Science 400
+// Title: Red Black Tree
+// Files: SearchTreeADT.java, RedBlackTree.java, IllegalKeyException.java, WordCountRunner.java
+// Course: Computer Science 400
 //
-//Author: Sam Govier
-//Email: sgovier@wisc.edu
-//Lecturer's Name: Andrew Kuemmel
+// Author: Sam Govier
+// Email: sgovier@wisc.edu
+// Lecturer's Name: Andrew Kuemmel
 //
 ///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
 //
-//Students who get help from sources other than their partner must fully
-//acknowledge and credit those sources of help here. Instructors and TAs do
-//not need to be credited here, but tutors, friends, relatives, room mates,
-//strangers, and others do. If you received no outside help from either type
-//of source, then please explicitly indicate NONE.
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
 //
-//Persons: NONE
-//Online Sources: NONE
+// Persons: NONE
+// Online Sources: NONE
 //
 /////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 
 
 public class WordCountRunner {
-    private static final String INPUT_FILE = "test.txt";
-    
-    public static void main(String[] args) {
-        RedBlackTree<String, Integer> tree = new RedBlackTree<String, Integer>();
-        Scanner scnr;
-        scnr = new Scanner(new File(INPUT_FILE));
-        
-        // loop through all lines
-           String line = scnr.nextLine();
+  private static final String INPUT_FILE = "test.txt";
 
-                // clean a line
-                // .replaceAll("[^a-zA-Z ]", "") removes all non-alphabetic characters (replaces them with the 
-                // empty string) and .split("\\s+") separates out the words using whitespace as a delimiter
-                
-                // this code should work without any modification
-                String[] words = line.trim().replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
-                
-                
-                // for each word in words that has a length > 0
-                   // add word to tree OR if the word already exists increment the count
-                
-        tree.printSideways();
-        // after all words are read in, print out this message
-       System.out.println("The tree has a size of " + tree.getSize() + " and a height of " + tree.getHeight());
-        
-        // prompt user for words until they enter 'qqq'
-        // below are lines of code you can use in your program
-        
-        Scanner input = new Scanner(System.in);
-        String request = "";
-        System.out.print("Enter a word to search for in \"" + INPUT_FILE + "\": ");
-        request = input.next().replaceAll("[^a-zA-Z ]", "").toLowerCase();
-        System.out.println("\"" + request + "\" was found in the file " + tree.getValue(request) + " times.");
-        System.out.println("Exiting.");
-        System.out.println("\"" + request + "\" was not found in the given file.");
-    
+  public static void main(String[] args) {
+    RedBlackTree<String, Integer> tree = new RedBlackTree<String, Integer>();
+    Scanner scnr;
+
+    try {
+      scnr = new Scanner(new File(INPUT_FILE));
+
+      while (scnr.hasNextLine()) {
+        // loop through all lines
+        String line = scnr.nextLine();
+
+        // clean a line with regex
+        // .replaceAll("[^a-zA-Z ]", "") removes all non-alphabetic characters (replaces them with
+        // the
+        // empty string) and .split("\\s+") separates out the words using whitespace as a delimiter
+        String[] words = line.trim().replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+
+        for (String word : words) {
+          if (word.length() > 0) {
+            try {
+              Integer value = tree.getValue(word);
+              if (value == null) {
+                tree.insert(word, 1);
+              } else {
+                tree.insert(word, value++);
+              }
+
+            } catch (IllegalKeyException e1) {
+              System.out.println("null word on the following line: " + line);
+            }
+          }
+        }
+
+      }
+
+      // close the scanner to prevent memory leaks
+      scnr.close();
+    } catch (FileNotFoundException e1) {
+      // if the file isn't found, exit
+      System.out.println("File not found.");
+      return;
     }
+    tree.printSideways();
+    // after all words are read in, print out this message
+    System.out.println(
+        "The tree has a size of " + tree.getSize() + " and a height of " + tree.getHeight());
+    
+    Scanner input = new Scanner(System.in);
+    String request = "";
+    // while the input doesn't match 'qqq', continue word lookup
+    do {
+      System.out.print("Enter a word to search for in \"" + INPUT_FILE + "\": ");
+      request = input.next().replaceAll("[^a-zA-Z ]", "").toLowerCase();
+      
+      if (request == "qqq") {
+        System.out.println("Exiting.");
+      }
+      else {
+        try {
+          Integer wordTimes = tree.getValue(request);
+          
+          if (null == wordTimes) {
+            System.out.println("\"" + request + "\" was not found in the given file.");
+          }
+          else {
+            System.out.println("\"" + request + "\" was found in the file " + wordTimes + " times.");
+          }
+        } catch (IllegalKeyException e1) {
+          System.out.println("Request was null.");
+        }
+      }
+    } while (request != "qqq");
+
+    // close the scanner to prevent memory leaks
+    input.close();
+  }
 }

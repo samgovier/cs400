fefb9656ef001503e0800d3e0940f238d2432a2a
samgovier <samgovier@gmail.com>
2019-10-21 20:39:40 -0500

    program5, implement HashTableADT

--- program5/src/HashTable.java
+++ program5/src/HashTable.java
@@ -2,81 +2,112 @@ import HashTable.HashNode;
 
 /**
  * Hash table data structure.
+ * 
  * @param <K> key
  * @param <V> value
  */
 @SuppressWarnings("unchecked")
 public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V> {
 
-	/**
-	 * Hash node inner class storing a key, value, and pointer to the next node in the chain.
-	 */
-	@SuppressWarnings("hiding")
-	private class HashNode<K, V> {
-		private K key;
-		private V value;
-		private HashNode<K, V> next;
+  /**
+   * Hash node inner class storing a key, value, and pointer to the next node in the chain.
+   */
+  @SuppressWarnings("hiding")
+  private class HashNode<K, V> {
+    private K key;
+    private V value;
+    private HashNode<K, V> next;
 
-		private HashNode(K key, V value, HashNode<K, V> next) {
-			this.key = key;
-			this.value = value;
-			this.next = next;
-		}
+    private HashNode(K key, V value, HashNode<K, V> next) {
+      this.key = key;
+      this.value = value;
+      this.next = next;
+    }
 
-		private String hashNodeToString() {
-			return key + " : " + value;
-		}
-	}
+    private String hashNodeToString() {
+      return key + " : " + value;
+    }
+  }
 
-	// instance variables for hash table
-	private double loadFactorThreshold;
-	private int numKeys;
-	private HashNode<K, V>[] table; // array of header nodes
+  // instance variables for hash table
+  private double loadFactorThreshold;
+  private int numKeys;
+  private HashNode<K, V>[] table; // array of header nodes
 
-	/**
-	 * Default no-arg constructor.
-	 */
-	public HashTable() {
-		this(5, 0.75);
-	}
+  /**
+   * Default no-arg constructor.
+   */
+  public HashTable() {
+    this(5, 0.75);
+  }
 
-	/**
-	 * Constructor with initial capacity and load factor threshold provided.
-	 * @param initialCapacity
-	 * @param loadFactorThreshold
-	 */
-	public HashTable(int initialCapacity, double loadFactorThreshold) {
-		this.loadFactorThreshold = loadFactorThreshold;
-		this.numKeys = 0;
-		// initialize buckets
-		table = (HashNode<K, V>[]) new HashNode[initialCapacity];
-	}
+  /**
+   * Constructor with initial capacity and load factor threshold provided.
+   * 
+   * @param initialCapacity
+   * @param loadFactorThreshold
+   */
+  public HashTable(int initialCapacity, double loadFactorThreshold) {
+    this.loadFactorThreshold = loadFactorThreshold;
+    this.numKeys = 0;
+    // initialize buckets
+    table = (HashNode<K, V>[]) new HashNode[initialCapacity];
+  }
 
-	// TODO: implement methods according to HashTableADT interface
+  // TODO: implement methods according to HashTableADT interface
+  @Override
+  public void insert(K key, V value) throws DuplicateKeyException, NullKeyException {
+    // TODO Auto-generated method stub
 
-	/**
-	 * Prints the HashTable to standard output.
-	 */
-	public void printHashTable() {
-		System.out.println("HASH TABLE:");
-		// print out an array of BucketList<KeyValuePair>
-		for (int i = 0; i < table.length; i++) {
-			System.out.print(i + ": ");
-			HashNode<K, V> currNode = table[i];
-			while (currNode != null) {
-				System.out.print("(" + currNode.hashNodeToString() + "), ");
-				currNode = currNode.next;
-			}
-			System.out.println();
-		}
-		System.out.println("numKeys: " + numKeys + "\tload factor: " + 1.0 * this.numKeys/this.table.length);
-		System.out.println("--------------------------------------------------");
-	}
+  }
 
-	/**
-	 * Main method.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	}
+  @Override
+  public boolean remove(K key) throws NullKeyException {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public V get(K key) throws KeyNotFoundException, NullKeyException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int numKeys() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public double getLoadFactor() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int getCapacity() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  /**
+   * Prints the HashTable to standard output.
+   */
+  public void printHashTable() {
+    System.out.println("HASH TABLE:");
+    // print out an array of BucketList<KeyValuePair>
+    for (int i = 0; i < table.length; i++) {
+      System.out.print(i + ": ");
+      HashNode<K, V> currNode = table[i];
+      while (currNode != null) {
+        System.out.print("(" + currNode.hashNodeToString() + "), ");
+        currNode = currNode.next;
+      }
+      System.out.println();
+    }
+    System.out.println(
+        "numKeys: " + numKeys + "\tload factor: " + 1.0 * this.numKeys / this.table.length);
+    System.out.println("--------------------------------------------------");
+  }
 }
---
+++ program5/src/NullKeyException.java
@@ -0,0 +1,4 @@
+
+public class NullKeyException extends Exception {
+
+}

acd60cacc91b7674bd8a16a1c7902ce8f4be6eed
samgovier <samgovier@gmail.com>
2019-10-25 19:46:00 -0500

    program5, exception classes

--- program5/src/KeyNotFoundException.java
+++ program5/src/KeyNotFoundException.java
@@ -1,4 +1,21 @@
-
+/**
+ * Checked exception thrown when a key can't be found
+ */
 public class KeyNotFoundException extends Exception {
 
+  /**
+   * Constructor for a KeyNotFoundException with an input message
+   * 
+   * @param message is the error message
+   */
+  public KeyNotFoundException(String message) {
+    super(message);
+  }
+  
+  /**
+   * Constructor for a KeyNotFoundException with default message
+   */
+  public KeyNotFoundException() {
+    super("Key not found");
+  }
 }
--- program5/src/NullKeyException.java
+++ program5/src/NullKeyException.java
@@ -1,4 +1,21 @@
-
+/*
+ * Checked exception thrown when a user attempts to insert or get a null key.
+ */
 public class NullKeyException extends Exception {
 
+  /**
+   * Constructor for a NullKeyException with an input message
+   * 
+   * @param message is the error message
+   */
+  public NullKeyException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructor for a NullKeyException with a default message
+   */
+  public NullKeyException() {
+    super("Key is null");
+  }
 }

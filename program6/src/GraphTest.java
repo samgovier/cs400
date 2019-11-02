d4fba139a04ac07a29b3af3dbd26c5a3946b59a4
samgovier <samgovier@gmail.com>
2019-11-02 18:01:39 -0500

    program6, header and formatting

--- program6/src/GraphTest.java
+++ program6/src/GraphTest.java
@@ -1,8 +1,29 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title: Graph
+// Files: Graph.java, GraphADT.java, GraphTest.java,
+// Person.java, SocialNetwork.java
+// Course: Computer Science 400
+//
+// Author: Sam Govier
+// Email: sgovier@wisc.edu
+// Lecturer's Name: Andrew Kuemmel
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-
 import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -17,55 +38,58 @@ import org.junit.runners.MethodSorters;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GraphTest {
-	private Graph<String> graph;
+  private Graph<String> graph;
 
-	@Rule
-	public Timeout globalTimeout = new Timeout(2000, TimeUnit.MILLISECONDS);
+  @Rule
+  public Timeout globalTimeout = new Timeout(2000, TimeUnit.MILLISECONDS);
 
-	@Before
-	public void setUp() throws Exception {
-		this.graph = new Graph<String>();
-	}
+  @Before
+  public void setUp() throws Exception {
+    this.graph = new Graph<String>();
+  }
 
-	@After
-	public void tearDown() throws Exception {
-		this.graph = null;
-	}
+  @After
+  public void tearDown() throws Exception {
+    this.graph = null;
+  }
 
-	@Test
-	public final void test00_addNullVertex() {
-		try {
-			graph.addVertex(null);
-			fail("test00: failed - should throw an IllegalArgumentException upon adding a null vertex");
-		} catch (IllegalArgumentException e) {
-			// test passed
-		} catch (Exception e) {
-			fail("test00: failed - unexpected exception occurred");
-		}
-	}
+  @Test
+  public final void test00_addNullVertex() {
+    try {
+      graph.addVertex(null);
+      fail("test00: failed - should throw an IllegalArgumentException upon adding a null vertex");
+    } catch (IllegalArgumentException e) {
+      // test passed
+    } catch (Exception e) {
+      fail("test00: failed - unexpected exception occurred");
+    }
+  }
 
-	@Test
-	public final void test01_removeNonexistentVertex() {
-		try {
-			graph.removeVertex("a");
-		} catch (Exception e) {
-			fail("test01: failed - unexpected exception occurred");
-		}
-	}
+  @Test
+  public final void test01_removeNonexistentVertex() {
+    try {
+      graph.removeVertex("a");
+    } catch (Exception e) {
+      fail("test01: failed - unexpected exception occurred");
+    }
+  }
 
-	@Test
-	public final void test02_addEdge() {
-		try {
-			graph.addVertex("a");
-			assertEquals("test02: failed - vertex insertion error", true, graph.getAllVertices().contains("a"));
-			graph.addVertex("b");
-			assertEquals("test02: failed - vertex insertion error", true, graph.getAllVertices().contains("b"));
-			graph.addEdge("a", "b");
-			assertEquals("test02: failed - edge insertion error", true, graph.getAdjacentVerticesOf("a").contains("b"));
-		} catch (Exception e) {
-			fail("test02: failed - unexpected exception occurred");
-		}
-	}
-	
-	// TODO: add additional JUnit tests here
+  @Test
+  public final void test02_addEdge() {
+    try {
+      graph.addVertex("a");
+      assertEquals("test02: failed - vertex insertion error", true,
+          graph.getAllVertices().contains("a"));
+      graph.addVertex("b");
+      assertEquals("test02: failed - vertex insertion error", true,
+          graph.getAllVertices().contains("b"));
+      graph.addEdge("a", "b");
+      assertEquals("test02: failed - edge insertion error", true,
+          graph.getAdjacentVerticesOf("a").contains("b"));
+    } catch (Exception e) {
+      fail("test02: failed - unexpected exception occurred");
+    }
+  }
+
+  // TODO: add additional JUnit tests here, 5 OR MORE
 }
--- program6/src/SocialNetwork.java
+++ program6/src/SocialNetwork.java
@@ -1,8 +1,30 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title: Graph
+// Files: Graph.java, GraphADT.java, GraphTest.java,
+// Person.java, SocialNetwork.java
+// Course: Computer Science 400
+//
+// Author: Sam Govier
+// Email: sgovier@wisc.edu
+// Lecturer's Name: Andrew Kuemmel
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Collections;
 import java.util.List;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,56 +33,58 @@ import org.json.simple.parser.JSONParser;
  * Represents a social network.
  */
 public class SocialNetwork {
-	private static Graph<String> graph;
-	private static String filename = "social-network.json";
+  private static Graph<String> graph;
+  private static String filename = "social-network.json";
 
-	/**
-	 * Parses the input JSON file.
-	 * @return array of Person objects which stores information about a single person
-	 * @throws Exception like FileNotFound, JsonParseException
-	 */
-	private static Person[] parseJSON() throws Exception {
-		// array storing the Person objects created from the JSON file to be loaded later in the graph
-		Person[] people = null;
-		
-		// TODO: create People objects by parsing JSON file and add them to array to be returned
-		
-		return people;
-	}
+  /**
+   * Parses the input JSON file.
+   * 
+   * @return array of Person objects which stores information about a single person
+   * @throws Exception like FileNotFound, JsonParseException
+   */
+  private static Person[] parseJSON() throws Exception {
+    // array storing the Person objects created from the JSON file to be loaded later in the graph
+    Person[] people = null;
 
-	/**
-	 * Construct an undirected graph from array of Person objects.
-	 * @param people an array of People objects generated from a json file
-	 */
-	private static void constructGraph(Person[] people) {
-		for (Person person : people) {
-			graph.addVertex(person.getName());
-			for (String friend : person.getFriends()) {
-				graph.addVertex(friend);
-				graph.addEdge(person.getName(), friend);
-			}
-		}
-	}
+    // TODO: create People objects by parsing JSON file and add them to array to be returned
 
-	public static void main(String[] args) {
-		if (args.length > 0) {
-			filename = args[0];	// allows alternate filename to be passed in through args
-		}
+    return people;
+  }
 
-		graph = new Graph<String>();
-		try {
-			Person[] persons = parseJSON();
-			constructGraph(persons);
-			List<String> people = graph.getAllVertices();
-			Collections.sort(people);
-			System.out.println("Network: " + people);
-			for (String person : people) {
-				List<String> friends = graph.getAdjacentVerticesOf(person);
-				Collections.sort(friends);
-				System.out.println(person + "'s friends: " + friends);
-			}
-		} catch (Exception e) {
-			System.out.println("Error: An unexpected exception occurred");
-		}
-	}
+  /**
+   * Construct an undirected graph from array of Person objects.
+   * 
+   * @param people an array of People objects generated from a json file
+   */
+  private static void constructGraph(Person[] people) {
+    for (Person person : people) {
+      graph.addVertex(person.getName());
+      for (String friend : person.getFriends()) {
+        graph.addVertex(friend);
+        graph.addEdge(person.getName(), friend);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    if (args.length > 0) {
+      filename = args[0]; // allows alternate filename to be passed in through args
+    }
+
+    graph = new Graph<String>();
+    try {
+      Person[] persons = parseJSON();
+      constructGraph(persons);
+      List<String> people = graph.getAllVertices();
+      Collections.sort(people);
+      System.out.println("Network: " + people);
+      for (String person : people) {
+        List<String> friends = graph.getAdjacentVerticesOf(person);
+        Collections.sort(friends);
+        System.out.println(person + "'s friends: " + friends);
+      }
+    } catch (Exception e) {
+      System.out.println("Error: An unexpected exception occurred");
+    }
+  }
 }

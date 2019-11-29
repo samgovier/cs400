53e0ac4bcec48f7873bec514149b6ca02394d989
samgovier <samgovier@gmail.com>
2019-11-29 13:38:57 -0600

    FinalProject, ADT, constructor and fields

--- hrtoons/src/tree/BTree.java
+++ hrtoons/src/tree/BTree.java
@@ -3,7 +3,8 @@ package tree;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class BTree<K extends Comparable<K>, V> {
+public class BTree<K extends Comparable<K>, V> implements BTreeADT<K,V> {
+  
   private class BNode {
     
     // keyList is the list of keys in this node
@@ -22,6 +23,58 @@ public class BTree<K extends Comparable<K>, V> {
       this.childrenList = new ArrayList<BNode>();
     }
   }
+
+  private BNode root;
+  private int size;
   
-  // MAKE SURE TO SORT WHEN ADDING/SUBTRACTING
+  public BTree() {
+    root = null;
+    size = 0;
+  }
+  @Override
+  public void insert(K key, V value) {
+    // TODO Auto-generated method stub
+
+    // MAKE SURE TO SORT WHEN ADDING/SUBTRACTING
+    
+  }
+
+  @Override
+  public boolean remove(K key) {
+    // TODO Auto-generated method stub
+
+    // MAKE SURE TO SORT WHEN ADDING/SUBTRACTING
+    return false;
+  }
+
+  @Override
+  public V getValue(K key) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean contains(K key) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public int height() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int size() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public ArrayList<V> getAllKeys() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
 }

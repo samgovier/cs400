978c38942ace6b5791f10b1bfaab21997f58d581
samgovier <samgovier@gmail.com>
2019-11-28 19:47:12 -0600

    FinalProject, create inner class

--- hrtoons/src/tree/BTree.java
+++ hrtoons/src/tree/BTree.java
@@ -1,5 +1,27 @@
 package tree;
 
-public class BTree {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class BTree<K extends Comparable<K>, V> {
+  private class BNode {
+    
+    // keyList is the list of keys in this node
+    private ArrayList<K> keyList;
+    
+    // valueMap is the map of keys to values
+    private HashMap<K,V> valueMap;
+    
+    // childrenList is the list of children in this node
+    private ArrayList<BNode> childrenList;
+    
+    // this constructor initializes the internal values
+    private BNode() {
+      this.keyList = new ArrayList<K>();
+      this.valueMap = new HashMap<K,V>();
+      this.childrenList = new ArrayList<BNode>();
+    }
+  }
+  
+  // MAKE SURE TO SORT WHEN ADDING/SUBTRACTING
 }

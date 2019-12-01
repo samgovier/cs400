aa2cf651ba2df695f3972ee070a453e1de09229c
samgovier <samgovier@gmail.com>
2019-11-30 22:18:28 -0600

    FinalProject, BTree contains and partial GetValue

--- hrtoons/src/tree/BTree.java
+++ hrtoons/src/tree/BTree.java
@@ -3,40 +3,62 @@ package tree;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class BTree<K extends Comparable<K>, V> implements BTreeADT<K,V> {
-  
+public class BTree<K extends Comparable<K>, V> implements BTreeADT<K, V> {
+
   private class BNode {
-    
+
     // keyList is the list of keys in this node
     private ArrayList<K> keyList;
-    
+
     // valueMap is the map of keys to values
-    private HashMap<K,V> valueMap;
-    
+    private HashMap<K, V> valueMap;
+
     // childrenList is the list of children in this node
     private ArrayList<BNode> childrenList;
-    
+
     // this constructor initializes the internal values
     private BNode() {
       this.keyList = new ArrayList<K>();
-      this.valueMap = new HashMap<K,V>();
+      this.valueMap = new HashMap<K, V>();
       this.childrenList = new ArrayList<BNode>();
+    }
+
+    /**
+     * this recursive method which finds the height from this node as the root
+     * 
+     * @return the height of the tree as an integer. If the node has no children, return 1
+     */
+    private int getHeight() {
+      // if there are no children, return 1
+      if (this.childrenList.size() == 0) {
+        return 1;
+      }
+
+      int maxHeight = 0;
+
+      for (BNode child : this.childrenList) {
+        maxHeight = Math.max(maxHeight, child.getHeight());
+      }
+
+      return 1 + maxHeight;
     }
   }
 
   private BNode root;
   private int size;
-  
+
   public BTree() {
     root = null;
     size = 0;
   }
+
   @Override
   public void insert(K key, V value) {
     // TODO Auto-generated method stub
 
     // MAKE SURE TO SORT WHEN ADDING/SUBTRACTING
-    
+    // INCREMENT SIZE
+
   }
 
   @Override
@@ -44,31 +66,91 @@ public class BTree<K extends Comparable<K>, V> implements BTreeADT<K,V> {
     // TODO Auto-generated method stub
 
     // MAKE SURE TO SORT WHEN ADDING/SUBTRACTING
+    // DECREMENT SIZE
     return false;
   }
 
   @Override
   public V getValue(K key) {
-    // TODO Auto-generated method stub
-    return null;
+
+    return getValue(key, root);
+  }
+
+  private V getValue(K key, BNode current) {
+
+    if (current.keyList.contains(key)) {
+      return true;
+    }
+
+    if (current.childrenList.size() == 2) {
+      K singleKey = current.keyList.get(0);
+      if (key.compareTo(singleKey) > 0) {
+        return getValue(key, current.childrenList.get(1));
+      }
+      return getValue(key, current.childrenList.get(0));
+    }
+
+    if (current.childrenList.size() == 3) {
+      if (key.compareTo(current.keyList.get(0)) < 0) {
+        return getValue(key, current.childrenList.get(0));
+      }
+      if (key.compareTo(current.keyList.get(1)) > 0) {
+        return getValue(key, current.childrenList.get(2));
+      }
+
+      return getValue(key, current.childrenList.get(1));
+    }
+
+    throw new KeyNotFoundException("Key wasn't found in tree.");
   }
 
   @Override
   public boolean contains(K key) {
-    // TODO Auto-generated method stub
+
+    return contains(key, root);
+  }
+
+  private boolean contains(K key, BNode current) {
+
+    if (current.keyList.contains(key)) {
+      return true;
+    }
+
+    if (current.childrenList.size() == 2) {
+      K singleKey = current.keyList.get(0);
+      if (key.compareTo(singleKey) > 0) {
+        return contains(key, current.childrenList.get(1));
+      }
+      return contains(key, current.childrenList.get(0));
+    }
+
+    if (current.childrenList.size() == 3) {
+      if (key.compareTo(current.keyList.get(0)) < 0) {
+        return contains(key, current.childrenList.get(0));
+      }
+      if (key.compareTo(current.keyList.get(1)) > 0) {
+        return contains(key, current.childrenList.get(2));
+      }
+
+      return contains(key, current.childrenList.get(1));
+    }
+
     return false;
   }
 
   @Override
   public int height() {
-    // TODO Auto-generated method stub
-    return 0;
+
+    if (null == root) {
+      return 0;
+    }
+
+    return root.getHeight();
   }
 
   @Override
   public int size() {
-    // TODO Auto-generated method stub
-    return 0;
+    return size;
   }
 
   @Override
@@ -76,5 +158,5 @@ public class BTree<K extends Comparable<K>, V> implements BTreeADT<K,V> {
     // TODO Auto-generated method stub
     return null;
   }
-  
+
 }

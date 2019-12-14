f175275d6d30854ad44b1913e7790ce61433efbc
samgovier <samgovier@gmail.com>
2019-12-14 16:32:25 -0600

    FinalProject, hrtoon csv and object

---
+++ hrtoons/src/application/hrtoon.java
@@ -0,0 +1,54 @@
+package application;
+
+public class hrtoon {
+  private Integer id;
+  private String title;
+  private String releaseDate;
+  private String dow;
+  private String type;
+  private Integer easterEggs;
+  private double runtime;
+  
+  public Integer getId() {
+    return id;
+  }
+  public void setId(Integer id) {
+    this.id = id;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getReleaseDate() {
+    return releaseDate;
+  }
+  public void setReleaseDate(String releaseDate) {
+    this.releaseDate = releaseDate;
+  }
+  public String getDow() {
+    return dow;
+  }
+  public void setDow(String dow) {
+    this.dow = dow;
+  }
+  public String getType() {
+    return type;
+  }
+  public void setType(String type) {
+    this.type = type;
+  }
+  public Integer getEasterEggs() {
+    return easterEggs;
+  }
+  public void setEasterEggs(Integer easterEggs) {
+    this.easterEggs = easterEggs;
+  }
+  public double getRuntime() {
+    return runtime;
+  }
+  public void setRuntime(double runtime) {
+    this.runtime = runtime;
+  }
+}

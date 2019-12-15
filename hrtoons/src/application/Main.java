570cac62fa49c8ce4be610ae4d975c5944b4f61b
samgovier <samgovier@gmail.com>
2019-12-15 14:31:07 -0600

    Final Project, add headers

--- hrtoons/src/application/Main.java
+++ hrtoons/src/application/Main.java
@@ -1,3 +1,27 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title: Final Project
+// Files: BTree.java, BtreeADT.java, BTreeTests.java,
+// DuplicateKeyException.java, KeyNotFoundException.java, NullKeyException.java,
+// Main.java, MainHelper.java, hrtoon.java
+// Course: Computer Science 400
+//
+// Author: Sam Govier
+// Email: sgovier@wisc.edu
+// Lecturer's Name: Andrew Kuemmel
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 package application;
 
 import java.util.ArrayList;
